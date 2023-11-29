6200ea3b858c13ffd0a4330e3e6d349c8448f380
OMOLLO PINCHEZ <masimevanistelrooy@gmail.com>
2023-11-29 13:40:29 +0300

    new

--- src/App.js
+++ src/App.js
@@ -2,26 +2,10 @@ import Axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [generatedExuses, setGenaratedExcuses] = useState("");
  
 
-  const FetchExcuse = (excuse) => {
-    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then((res) => {
-      setGenaratedExcuses(res.data[0].excuse);
-    });
-  };
-
-  
-
   return (
-    <div className="App">
-      <h1>Generate An Excuse</h1>
-      <button onClick={()=>FetchExcuse("party")}>Party</button>
-      <button onClick={()=>FetchExcuse("family")}>Family</button>
-      <button onClick={()=>FetchExcuse("office")}>Office</button>
-
-      <p>{generatedExuses}  </p>
-    </div>
+    <div className="App"></div>
   );
 }
 
